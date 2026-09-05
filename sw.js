b9408aa2da394281da4a36f59e6b475d6951a662
futuregoal <shubhamrao0807@gmail.com>
2026-09-05 13:28:46 -0700

    Add in-app YouTube video player

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'future-v1';
+const CACHE = 'future-v2';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
